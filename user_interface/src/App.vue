1f4abf9af7f41e2f812153a4f2a4984b54ce6054
danielbinschmid <danielbinschmid@outlook.de>
2023-01-07 01:09:55 +0100

    User interface prototype simulation basis.

--- user_interface/src/App.vue
+++ user_interface/src/App.vue
@@ -1,70 +1,105 @@
 <script setup lang="ts">
-import { onMounted, reactive } from "vue";
-import { dummyHTTPSGet } from "./api";
+import { onMounted, reactive, ref } from "vue";
 
+const LOGO_RELATIVE_HEIGHT = 0.2;
+const SCENE_IMG_RELATIVE_SIZE = 0.7;
 
-// reactive variables
-const counter = reactive({ value: 0 });
+const scene = ref(null);
+const sceneImgGhost = ref(null);
 
-let initPlanet: Planet = {
-    url: "-",
-    explanation: "-"
-}
-const planetInfo = reactive(initPlanet); 
+const logo = reactive({ height: 50 });
+const windowSizes = reactive({ width: window.innerWidth, height: window.innerHeight });
+const sceneSizes = reactive({ width: window.innerWidth, height: window.innerHeight });
 
-// called on mount
 onMounted(() => {
-    setInterval(() => {
-        counter.value ++;
-    }, 1000); 
+    let sceneImage: HTMLImageElement = sceneImgGhost.value;
+    sceneImage.onload = () => {
+        computeSizes();
+    }
 });
 
+function computeSizes() {
+    let ratioScene = sceneImgGhost.value.width / sceneImgGhost.value.height;
+    let ratioWindow = window.innerWidth / window.innerHeight;
 
-function getDummy(): void {
-    dummyHTTPSGet().then(value => {
-        initPlanet.url = value.url;
-        initPlanet.explanation = value.explanation;
-    });
+    if (ratioScene > ratioWindow) {
+        sceneSizes.width = window.innerWidth * SCENE_IMG_RELATIVE_SIZE;
+        sceneSizes.height = Math.floor(sceneSizes.width / ratioScene);
+    } else {
+        sceneSizes.height = window.innerHeight * SCENE_IMG_RELATIVE_SIZE;
+        sceneSizes.width = Math.floor(ratioScene * sceneSizes.height);
+    }
+
+    logo.height = LOGO_RELATIVE_HEIGHT * sceneSizes.height;
 }
 
-function clearPlanetInfo(): void { 
-    initPlanet.url = "-";
-    initPlanet.explanation = "-";
-}
+window.onresize = () => { if (sceneImgGhost.value.complete) computeSizes(); }
 
 </script>
 
 <template>
     <main>
 
-        <div class="canvas-container"> 
-            <canvas id="canvas"> </canvas>
+        <div class="ghost">
+            <img src='@/assets/road_scene.png' ref="sceneImgGhost" />
         </div>
-        
-        
-        
-        <div> 
-            Time since deployment: {{ counter.value }}
-        </div>
-        <div>
-            Planet url: {{ planetInfo.url }}
-        </div>
-        <div>
-            {{ planetInfo.explanation }}
-        </div>
+        <div class="globalContainer" :style="{ width: windowSizes.width, height: windowSizes.height }">
+            <div class="headerContainer" :style="{ width: sceneSizes.width + 'px' }">
+                <h1 class="headerItem">
+                    PROTOTYPE SIMULATION
+                </h1>
+                <div class="headerItem">
+                    <div class="logoWrapper">
+                        <img src='@/assets/coop-logo.png' :height="logo.height" />
+                    </div>
+                </div>
+                <h1 class="headerItem">
+                    HCI FOR AUTONOMOUS DRIVING
+                </h1>
+            </div>
 
-        <button @click="getDummy()">
-            Click me to get planet information
-        </button>
-        <button @click="clearPlanetInfo()">
-            Clear planet information
-        </button>
-
+            <div class="container">
+                <img src='@/assets/road_scene.png' ref="scene" :width="sceneSizes.width" :height="sceneSizes.height" />
+            </div>
+        </div>
     </main>
 </template>
 
 <style scoped>
-.canvas-container {
+
+.ghost {
+    position: fixed;
+    visibility: hidden;
+}
+
+.logoWrapper {
+    display: flex;
+    justify-content: center;
+}
+
+.headerItem {
+    flex-basis: 33%;
+    display: flex;
+    justify-content: center;
+    flex-flow: column;
+}
+
+h1 {
+    font-size: medium;
+    margin-top: auto;
+    margin-bottom: auto;
+    text-align: center;
+    font-weight: 100;
+}
+
+.headerContainer {
+    display: flex;
+    justify-content: space-around;
+    flex-flow: row;
+    align-content: center;
+}
+
+.globalContainer {
     position: fixed;
     top: 0;
     left: 0;
@@ -74,10 +109,13 @@ function clearPlanetInfo(): void {
     flex-flow: column;
     justify-content: center;
     align-items: center;
+    background-color: rgb(231, 229, 225);
 }
 
-#canvas {
-
-    background: inherit;
+.container {
+    display: flex;
+    align-content: center;
+    justify-content: center;
 }
+
 </style>
