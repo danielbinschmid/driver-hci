ae4482875237fb6f10e5858d36a86d1b7bbda9a9
danielbinschmid <danielbinschmid@outlook.de>
2023-01-13 14:39:03 +0100

    basic server

--- user_interface/src/electron/main/main.ts
+++ user_interface/src/electron/main/main.ts
@@ -7,7 +7,56 @@ import {
     ipcMain,
     MenuItem
 } from 'electron';
-import { startListening } from './server';
+
+import express from "express";
+import bodyParser from "body-parser"
+
+function startListening() {
+    
+    
+    
+    const router = express.Router();
+    const app = express();
+
+
+    const port = 3001
+    console.log("here")
+    
+    app.get('/', (req, res) => {
+    res.send('Hello World!')
+    })
+
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    })
+
+    /**
+     * 
+    
+    // add router in express app
+    app.use("/",router);
+
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+
+    router.get('/',(req, res) => {
+        res.sendFile("index.html");
+    });
+
+    router.post('/login',(req, res) => {
+        var user_name = req.body.user;
+        var password = req.body.password;
+        console.log("User name = "+user_name+", password is "+password);
+        res.end("yes");
+    });
+
+    app.listen(3000,() => {
+        console.log("now listening");
+    })
+     */
+
+
+}
 
 // https://www.electronjs.org/de/docs/latest/tutorial/ipc
 
@@ -71,7 +120,7 @@ app.whenReady().then(() => {
     });
 
     
-    // startListening();
+    startListening();
     
 });
 
