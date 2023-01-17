5a603cc2155efc586912cb0b78820aef11bdf2d5
danielbinschmid <danielbinschmid@outlook.de>
2023-01-17 22:19:20 +0100

    extended top bar with logo of landing page

--- landing-page/src/App.vue
+++ landing-page/src/App.vue
@@ -44,7 +44,19 @@ window.addEventListener(
     <v-main>
 
         <div class="top-bar" ref="topbar">
-        
+            <div class="headerContainer">
+                <h2 class="headerItem">
+                    PROTOTYPE SIMULATION
+                </h2>
+                <div class="headerItem">
+                    <div class="logoWrapper">
+                        <img src='./assets/coop-logo.png' :height="100" />
+                    </div>
+                </div>
+                <h2 class="headerItem">
+                    HCI FOR AUTONOMOUS DRIVING
+                </h2>
+            </div>
             <div class="tabs">
                 <v-tabs v-model="model_.val" >   
                     <div v-for="tab in tabs" :key="tab">
@@ -76,7 +88,29 @@ window.addEventListener(
 
 
 <style scoped>
+.headerContainer {
+    display: flex;
+    justify-content: space-around;
+    flex-flow: row;
+    align-content: center;
+}
+.logoWrapper {
+    display: flex;
+    justify-content: center;
+}
 
+h2{
+    font-size: medium;
+    text-align: center;
+    font-weight: 300;
+}
+.headerItem {
+    flex-basis: 33%;
+    display: flex;
+    justify-content: center;
+    flex-flow: column;
+    color: black;
+}
 section {
     width: 100vw;
     height: 80vh;
@@ -99,6 +133,8 @@ section {
 }
 
 .top-bar {
+    display: flex;
+    flex-flow: column;
     width: 100vw;
     height: fit-content;
     background-color: rgb(231, 229, 225);
